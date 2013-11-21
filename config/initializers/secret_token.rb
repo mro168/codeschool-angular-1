d3c08b130667edd53c71db0ae1deb3e9ef1b138e
Mario Stefano <me@mario.sg>
2013-11-21 09:43:38 +0700

    initialize rails apps fix database config initialize bower fix bower directory install angular bower install angular resource bower install high voltage gem create index high voltage page fix route
    --Mario

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularCodeschool::Application.config.secret_key_base = '8446fafdfd4d52455b67c4b0cfb20be59aebdd5180960dccc50fd13f7ec4b5724830018e7af6c11a3f84b4cc918de7c6621898155d2da50732b3fc8671b1acc5'
